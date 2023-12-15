b53a337e0f7a9ab1a73ef4cb332d06832d5c822b
mandalavijaysurya <mandala.vijay.surya@gmail.com>
2023-12-16 01:41:03 +0530

    Done easy questions

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,24 @@
 */
 
 function isAnagram(str1, str2) {
-
+  str1 = str1.toLowerCase();
+  str2 = str2.toLowerCase();
+  let map = new Map()
+  
+  for(let char of str1) {
+    if(map.has(char)) map.set(char, map.get(char) + 1);
+    else map.set(char,1)
+  }
+  for(let char of str2) {
+    if(map.has(char) && map.get(char) > 0) map.set(char, map.get(char) - 1)
+    else return false
+  }
+  for(key of map.keys()){
+    if(map.get(key) > 0) return false
+  }
+  return true
 }
+
+console.log(isAnagram('Debit Card', 'Bad Credit'))
 
 module.exports = isAnagram;
--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -14,7 +14,53 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  let output = [];
+  let map = new Map();
+  transactions.forEach(element => {
+    if(map.has(element.category))  map.set(element.category, map.get(element.category) + element.price)
+    else map.set(element.category, element.price)
+  });
+  map.forEach((value, key) => output.push({category: key, totalSpent: value}))
+  return output
 }
+
+
+console.log(calculateTotalSpentByCategory([
+  {
+    id: 1,
+    timestamp: 1656076800000,
+    price: 10,
+    category: 'Food',
+    itemName: 'Pizza',
+  },
+  {
+    id: 2,
+    timestamp: 1656259600000,
+    price: 20,
+    category: 'Food',
+    itemName: 'Burger',
+  },
+  {
+    id: 3,
+    timestamp: 1656019200000,
+    price: 15,
+    category: 'Clothing',
+    itemName: 'T-Shirt',
+  },
+  {
+    id: 4,
+    timestamp: 1656364800000,
+    price: 30,
+    category: 'Electronics',
+    itemName: 'Headphones',
+  },
+  {
+    id: 5,
+    timestamp: 1656105600000,
+    price: 25,
+    category: 'Clothing',
+    itemName: 'Jeans',
+  },
+]))
 
 module.exports = calculateTotalSpentByCategory;
